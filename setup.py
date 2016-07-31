56a154079958594207a39458fc1a95e1fb5a6467
Nikul Ukani <nikul@e.columbia.edu>
2016-07-31 02:16:47 -0400

    Move all code to new directory; bump version for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
-from distutils.core import setup, Extension
-pyorient_native = Extension("pyorient_native", ["orientc_reader.cpp", "orientc_writer.cpp",
-                                     "helpers.cpp", "parse_exception.cpp",
-                                     "listener.cpp", "encoder.cpp",
-                                     "pyorient_native.cpp"],
-                    include_dirs = [],
-                    library_dirs = [],
+from setuptools import find_packages, setup, Extension
+
+h_files = ["pyorient_native/orientc_reader.h", "pyorient_native/orientc_writer.h",
+           "pyorient_native/orientc.h","pyorient_native/helpers.h","pyorient_native/parse_exception.h",
+                                            "pyorient_native/listener.h", "pyorient_native/encoder.h","pyorient_native/pendian.h"]
+pyorient_native = Extension("pyorient_native", sources=["pyorient_native/orientc_reader.cpp", "pyorient_native/orientc_writer.cpp",
+                                     "pyorient_native/helpers.cpp", "pyorient_native/parse_exception.cpp",
+                                     "pyorient_native/listener.cpp", "pyorient_native/encoder.cpp",
+                                     "pyorient_native/pyorient_native.cpp"],
+                            depends = h_files,
+                            library_dirs = [],
+                            include_dirs = ["./pyorient_native"],
                     language="c++", libraries=["stdc++"])
 setup(
     name = "pyorient_native",
-    version="1.0",
+    version="1.1.0",
     description="OrientDB Binary Serialization package for python",
     author="Nikul Ukani",
     author_email="nhu2001@columbia.edu",
-    ext_modules = [pyorient_native]
+    ext_modules = [pyorient_native],
+    packages=find_packages(),
+    url="https://github.com/nikulukani/pyorient_native",
     )
