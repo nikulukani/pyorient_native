bd388596e7ad60459e73d88ac2274923577de4f3
Nikul Ukani <nikul@ee.columbia.edu>
2017-07-06 02:53:42 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ pyorient_native = Extension("pyorient_native", sources=["pyorient_native/orientc
                     language="c++", libraries=["stdc++"])
 setup(
     name = "pyorient_native",
-    version="1.1.2",
+    version="1.1.3",
     description="OrientDB Binary Serialization package for python",
     author="Nikul Ukani",
     author_email="nhu2001@columbia.edu",
