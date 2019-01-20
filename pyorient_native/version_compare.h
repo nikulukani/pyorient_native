2b9c3290e95a51ce8faff3929a3da07214cf00cd
Sam Caldwell <mail@samcaldwell.net>
2019-01-20 15:50:21 -0800

    code cleanup now that things are working for version_compare

--- pyorient_native/version_compare.h
+++ pyorient_native/version_compare.h
@@ -1,10 +1,16 @@
+/* Sam Caldwell <mail@samcaldwell.net> (released public domain to the project where this code resides).
+ * Version Comparison Class.
+ *
+ * Given a version string in the form xxx.yyy.zzz.aaa, this class will parse the string
+ * and allow version comparison operators to test equivalence, etc. Expected behavior is
+ * defined in test_version_compare.cpp (in tests/ directory).
+ *
+ */
 #ifndef VERSION_COMPARE_H
 #define VERSION_COMPARE_H
 #endif
 #include <iostream>
 #include <string>
-
-#define DEBUG_FLAG
 
 using namespace std;
 
@@ -17,10 +23,6 @@ public:
 		this->revision = 0;
 		this->build = 0;
 		std::sscanf(version.c_str(), "%d.%d.%d.%d", &this->major, &this->minor, &this->revision, &this->build);
-		#ifdef DEBUG_FLAG
-		cout << "Version String (raw): "<< version.c_str() << "\n";
-		cout << "v1: " << this->major << "." << this->minor << "." << this->revision << "." << this->build << "\n";
-		#endif
 	}
 
 	bool operator<(const Version rhs){
--- tests/test_version_compare.cpp
+++ tests/test_version_compare.cpp
@@ -1,16 +1,19 @@
+/* Sam Caldwell <mail@samcaldwell.net> (released public domain to the project where this code resides).
+ * This file contains the tests for the version_compare.h source file.
+ */
 #include "../pyorient_native/version_compare.h"
 #include <assert.h>
 #include <iostream>
 
-void show_values(Version v1, Version v2){
+/*void show_values(Version v1, Version v2){
 	cout << "show_values():\n";
 	cout << "\tv1: " << v1.major << "." << v1.minor << "." << v1.revision << "." << v1.build << "\n";
 	cout << "\tv2: " << v2.major << "." << v2.minor << "." << v2.revision << "." << v2.build << "\n";
 	cout << "\t---\n";
-}
+}*/
 
 bool test_equivalence_operator_happy(){
-	cout << "\n\ntest: test_equivalence_operator_happy() starting\n";
+	cout << "test: test_equivalence_operator_happy() starting\n";
 	const char *version_string = "1.0.0";
 
 	Version v1(version_string);
@@ -20,81 +23,78 @@ bool test_equivalence_operator_happy(){
 }
 
 bool test_equivalence_operator_sad(){
-	cout << "\n\ntest: test_equivalence_operator_sad(): starting\n";
+	cout << "test: test_equivalence_operator_sad(): starting\n";
 	const char *version_string1 = "1.0.0";
 	const char *version_string2 = "2.0.0";
 
 	Version v1(version_string1);
 	Version v2(version_string2);
 
-	show_values(v1,v2);
-
 	return !(v1 == v2);
 }
 
 bool test_less_than_operator_happy(){
-	cout << "\n\ntest: test_less_than_operator_happy(): starting\n";
+	cout << "test: test_less_than_operator_happy(): starting\n";
 	const char *version_string1 = "1.0.0";
 	const char *version_string2 = "1.0.1";
 
 	Version v1(version_string1);
 	Version v2(version_string2);
 
-	show_values(v1,v2);
-
 	return (v1 < v2);
 }
 
 bool test_less_than_operator_sad_if_equal(){
-	cout << "\n\ntest: test_less_than_operator_sad_if_equal(): starting\n";
+	cout << "test: test_less_than_operator_sad_if_equal(): starting\n";
 	const char *version_string1 = "1.0.0";
 	const char *version_string2 = "1.0.0";
 
 	Version v1(version_string1);
 	Version v2(version_string2);
-
-	show_values(v1,v2);
 
 	return !(v1 < v2);
 }
 
 bool test_less_than_operator_sad_if_greater(){
-	cout << "\n\ntest: test_less_than_operator_sad_if_greater(): starting\n";
+	cout << "test: test_less_than_operator_sad_if_greater(): starting\n";
 	const char *version_string1 = "2.0.0";
 	const char *version_string2 = "1.0.0";
 
 	Version v1(version_string1);
 	Version v2(version_string2);
-
-	show_values(v1,v2);
 
 	return !(v1 < v2);
 }
 
 bool test_inequal_operator_happy(){
-	cout << "\n\ntest: test_less_than_operator_sad_if_greater(): starting\n";
+	cout << "test: test_less_than_operator_sad_if_greater(): starting\n";
 	const char *version_string1 = "2.0.0";
 	const char *version_string2 = "1.0.0";
 
 	Version v1(version_string1);
 	Version v2(version_string2);
 
-	show_values(v1,v2);
-
 	return (v1 != v2);
 }
 
 bool test_inequal_operator_sad(){
-	cout << "\n\ntest: test_less_than_operator_sad_if_greater(): starting\n";
+	cout << "test: test_less_than_operator_sad_if_greater(): starting\n";
 	const char *version_string1 = "1.0.0";
 	const char *version_string2 = "1.0.0";
 
 	Version v1(version_string1);
 	Version v2(version_string2);
 
-	show_values(v1,v2);
-
 	return !(v1 != v2);
+}
+
+bool test_casted_comparison_happy(){
+	cout << "test: test_casted_comparison_happy(): starting\n";
+	return (Version)("1.0") == (Version)("1.0");
+}
+bool test_casted_comparison_sad(){
+	cout << "test: test_casted_comparison_sad(): starting\n";
+	return (Version)("1.0") == (Version)("1.0");
 }
 
 int main(){
@@ -105,4 +105,6 @@ int main(){
 	assert (test_less_than_operator_happy());
 	assert (test_less_than_operator_sad_if_equal());
 	assert (test_less_than_operator_sad_if_greater());
+	assert (test_casted_comparison_happy());
+	assert (test_casted_comparison_sad());
 }
